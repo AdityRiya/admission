2a1170e1dd0f7bab70d5aadd10ff7a96aec9f0ab
AdityRiya <66531291+AdityRiya@users.noreply.github.com>
2021-12-05 16:38:05 +0600

    photo

--- js/script.js
+++ js/script.js
@@ -139,3 +139,11 @@ function showdivfour(){
 
   }
 }
+var loadFile = function(event) {
+  var reader = new FileReader();
+  reader.onload = function(){
+    var output = document.getElementById('output');
+    output.src = reader.result;
+  };
+  reader.readAsDataURL(event.target.files[0]);
+};
